7068ef5b207214b0830e6126f22bf01dc7adbcfc
Vítor Guerreiro <95621393+vsguerreiro@users.noreply.github.com>
2023-11-03 10:32:45 +0000

    Logic works; added CSS

--- prs.js
+++ prs.js
@@ -1,37 +1,51 @@
-game();RemotePlayback
-function game() {
-  // Keep track of the player's and computer's wins.
-  let playerWins = 0;
-  let computerWins = 0;
+// Keep track of the player's and computer's wins.
+let playerWins = 0;
+let computerWins = 0;
+let roundsPlayed = 0;
 
-  // Play five rounds of the game.
-  for (let i = 0; i < 5; i++) {
-    // Get the player's and computer's selections.
-    const playerSelection = prompt('Rock, paper, or scissors?');
-    const computerSelection = getComputerChoice();
+// Function to start the game and handle each round.
+function startGame() {
+  // Get the player's selection.
+  const choiceButtons = document.querySelectorAll(".choice-button");
+  choiceButtons.forEach((button) => {
+    button.addEventListener("click", function (e) {
+      const playerSelection = button.id;
 
-    // Determine the winner of the round.
-    const roundWinner = determineWinner(playerSelection, computerSelection);
+      // Get the computer's selection.
+      const computerSelection = getComputerChoice();
 
-    // Display the round result.
-    displayRoundResult(playerSelection, computerSelection, roundWinner);
+      // Check if the game has already ended (after 5 rounds).
+      if (roundsPlayed >= 5) {
+        return;
+      }
 
-    // Update the player's and computer's wins.
-    if (roundWinner === 'Player wins') {
-      playerWins++;
-    } else if (roundWinner === 'Computer wins') {
-      computerWins++;
-    }
-  }
+      // Increment the rounds played.
+      roundsPlayed++;
 
-  // Display the overall results of the game.
-  if (playerWins > computerWins) {
-    console.log('Player wins the game!');
-  } else if (computerWins > playerWins) {
-    console.log('Computer wins the game!');
-  } else {
-    console.log('The game is tied!');
-  }
+      //
+      // Determine the winner of the round.
+      const roundWinner = determineWinner(playerSelection, computerSelection);
+
+      // Display the round result in the DOM.
+      displayRoundResult(playerSelection, computerSelection, roundWinner);
+
+      // Update the player's and computer's wins.
+      if (roundWinner === "Player wins") {
+        playerWins++;
+      } else if (roundWinner === "Computer wins") {
+        computerWins++;
+      }
+
+      // Check if the game is over after this round.
+      if (roundsPlayed === 5) {
+        displayGameResults(playerWins, computerWins);
+        // Remove event listeners from choice buttons to prevent further selection.
+        choiceButtons.forEach((button) => {
+          button.removeEventListener("click", function (e) {});
+        });
+      }
+    });
+  });
 }
 
 function getComputerChoice() {
@@ -41,47 +55,72 @@ function getComputerChoice() {
   // Convert the random number to a rock, paper, or scissors selection.
   switch (choice) {
     case 0:
-      return 'rock';
+      return "rock";
     case 1:
-      return 'paper';
+      return "paper";
     case 2:
-      return 'scissors';
-    default:
-      // This should never happen.
-      return 'unknown';
+      return "scissors";
   }
 }
 
 function determineWinner(playerSelection, computerSelection) {
   // Compare the player's and computer's selections to determine the winner.
   if (playerSelection === computerSelection) {
-    return 'Tie';
-  } else if (playerSelection === 'rock') {
-    if (computerSelection === 'paper') {
-      return 'Computer wins';
+    return "Tie";
+  } else if (playerSelection === "rock") {
+    if (computerSelection === "paper") {
+      return "Computer wins";
     } else {
-      return 'Player wins';
+      return "Player wins";
     }
-  } else if (playerSelection === 'paper') {
-    if (computerSelection === 'scissors') {
-      return 'Computer wins';
+  } else if (playerSelection === "paper") {
+    if (computerSelection === "scissors") {
+      return "Computer wins";
     } else {
-      return 'Player wins';
+      return "Player wins";
     }
-  } else if (playerSelection === 'scissors') {
-    if (computerSelection === 'rock') {
-      return 'Computer wins';
+  } else if (playerSelection === "scissors") {
+    if (computerSelection === "rock") {
+      return "Computer wins";
     } else {
-      return 'Player wins';
+      return "Player wins";
     }
   } else {
     // This should never happen.
-    return 'Invalid selection';
+    return "Invalid selection";
   }
 }
 
 function displayRoundResult(playerSelection, computerSelection, roundWinner) {
-  console.log(`Player selected ${playerSelection}.`);
-  console.log(`Computer selected ${computerSelection}.`);
-  console.log(`${roundWinner}`);
+  // Display the round result in the DOM.
+  const resultsDiv = document.querySelector("#results");
+  resultsDiv.innerHTML += `
+  <p>Player selected ${playerSelection}.</p>
+  <p>Computer selected ${computerSelection}.</p>
+  <p>${roundWinner}</p>
+  `;
 }
+
+function displayGameResults(playerWins, computerWins) {
+  // Display the overall results of the game in the DOM.
+  const resultsDiv = document.querySelector("#results");
+  resultsDiv.innerHTML += `
+    <p>Player wins: ${playerWins}</p>
+    <p>Computer wins: ${computerWins}</p>
+  `;
+
+  // Determine the game winner.
+  let gameResult = "";
+  if (playerWins > computerWins) {
+    gameResult = "Congratulations! You win the game!";
+  } else if (computerWins > playerWins) {
+    gameResult = "Computer wins the game!";
+  } else {
+    gameResult = "It's a tie! No overall winner.";
+  }
+
+  resultsDiv.innerHTML += `<p>${gameResult}</p>`;
+}
+
+// Start the game when the first button is pressed.
+startGame();
